95449a88e8873c7fdb0183f776b2e52a5304ccbf
Augusto Araujo <augroberto@Augustos-Mac-mini.local>
2023-04-07 18:59:44 -0300

    database project

---
+++ Projeto-Final/database project/postcard/capas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[capas] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
+    [uf]      NVARCHAR (MAX) NOT NULL,
+    [musica]   NVARCHAR (MAX) NOT NULL,
+    [youtube] NVARCHAR (MAX) NOT NULL,
+    [imagem]   NVARCHAR (MAX) NOT NULL
+);
