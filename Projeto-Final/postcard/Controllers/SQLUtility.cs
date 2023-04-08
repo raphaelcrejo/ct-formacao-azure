19e1038261420cbfdafdc24cba2999be0f50f713
Augusto Araujo <augroberto@Augustos-Mac-mini.local>
2023-04-07 21:12:21 -0300

    add capas database

---
+++ Projeto-Final/postcard/Controllers/SQLUtility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using postcard.Models;
+using Microsoft.Data.SqlClient;
+
+namespace postcard.Controllers
+{
+	public class SQLUtility
+	{
+		public List<Capas> getcapas(string query, IConfiguration configuration)
+        {
+            var capas = new List<Capas>();
+
+			string connectionString = configuration.GetValue<string>("ConnectionStrings:SQLDatabase");
+
+			try
+			{
+				using (SqlConnection con = new SqlConnection(connectionString))
+				{
+					con.Open();
+					SqlCommand cmd = new SqlCommand(query);
+					cmd.Connection = con;
+
+					SqlDataReader rdr = cmd.ExecuteReader();
+					while (rdr.Read())
+					{
+						Capas capa = new Capas();
+
+						capa.id = Convert.ToInt32(rdr["id"]);
+                        capa.uf = rdr["uf"].ToString();
+                        capa.musica = rdr["musica"].ToString();
+                        capa.youtube = rdr["youtube"].ToString();
+                        capa.imagem = rdr["imagem"].ToString();
+
+						capas.Add(capa);
+                    }
+				}
+			}
+			catch (SqlException ex)
+			{
+				throw ex;
+            }
+            return (capas);
+		}
+	}
+}
+
---
+++ Projeto-Final/postcard/Models/Capas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace postcard.Models
+{
+	public class Capas
+	{
+		public int id { get; set; }
+		public string uf { get; set; }
+		public string musica { get; set; }
+		public string youtube { get; set; }
+		public string imagem { get; set; }
+
+		public string error { get; set; }
+	}
+}
+
