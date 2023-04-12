820239f2f673043a10c8b9bc0f2b190d92cc5dd4
Augusto Araujo <augroberto@Augustos-Mac-mini.local>
2023-04-12 01:47:03 -0300

    fileshare

---
+++ Aula 6/Projeto Pratico/File Share/Source/postcard/Models/FeatureToggle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Azure;
+using Azure.Data.Tables;
+
+namespace postcard.Models
+{
+	public class FeatureToggle : ITableEntity
+    {
+		public string PartitionKey { get; set; }
+		public string RowKey { get; set; }
+		public string ativo { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
+
