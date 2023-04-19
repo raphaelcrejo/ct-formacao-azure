21da05715ee526bd111a8e90aea5354fa51d2cb3
Augusto Araujo <augroberto@Augustos-Mac-mini.local>
2023-04-19 02:39:29 -0300

    add function

---
+++ Aula 8/Projeto Pratico/Source/web/postcard/Models/Estado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace postcard.Models
+{
+	public class Estado
+	{
+		public string estado { get; set; }
+
+		public List<PostCards> listpostcards { get; set; }
+
+        public string hostip { get; set; }
+    }
+}
+
