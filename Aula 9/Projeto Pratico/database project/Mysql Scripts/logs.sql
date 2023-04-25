14e6c7bc2f4a0ad666ca67f1e237b98cbc916b16
Augusto Araujo <augroberto@Augustos-Mac-mini.local>
2023-04-24 23:28:00 -0300

    aula 9

--- Aula 9/Projeto Pratico/database project/Mysql Scripts/logs.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA `downloadlogs` ;
-
-CREATE TABLE `downloadlogs`.`logs` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `host` VARCHAR(255) NULL,
-  `arquivo` VARCHAR(255) NULL,
-  `url` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`));
